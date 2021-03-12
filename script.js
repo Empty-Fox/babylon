685b572ba929a188e0a8aa8d06b131e57736aa92
Empty-Fox <ulyana250188@gmail.com>
2021-03-13 00:04:06 +0200

    scroll top button

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ $(document).ready(function(){
   });
 
   document.getElementById("year").innerHTML = new Date().getFullYear();
+
+
+  //Get the button
+  var mybutton = document.getElementById("scrollTop");    
+  window.onscroll = function() {scrollFunction()};  
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  function topFunction() {
+    window.scrollTo({top: 0, behavior: 'smooth'});    
+  }
